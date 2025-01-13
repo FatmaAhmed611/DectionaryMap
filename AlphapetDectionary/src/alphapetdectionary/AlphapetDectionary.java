30b3a9d836c804b7985b92a699b202358af5a930
Your Name <you@example.com>
2025-01-13 19:55:06 +0200

    change dictionary to one dimentional array:

--- AlphapetDectionary/src/alphapetdectionary/AlphapetDectionary.java
+++ AlphapetDectionary/src/alphapetdectionary/AlphapetDectionary.java
@@ -7,7 +7,9 @@ package alphapetdectionary;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -22,38 +24,32 @@ public class AlphapetDectionary {
     public static void main(String[] args) {
         // TODO code application logic here
         HashMap<String,ArrayList<String>> alphapet=new HashMap<>();
-        String[][] dictionary= {
-            {"a", "apple", "ant", "arrow"},
-            {"b", "banana", "bird", "boat"},
-            {"c", "cat", "car", "cup"},
-            {"d", "dog", "duck", "desk"},
-            {"e", "elephant", "egg", "eagle"},
-            {"f", "fish", "frog", "flag"},
-            {"g", "grapes", "goat", "guitar"},
-            {"h", "hat", "house", "horse"},
-            {"i", "ice", "island", "ink"},
-            {"j", "jelly", "jump", "jacket"},
-            {"k", "kite", "king", "kangaroo"},
-            {"l", "lion", "lamp", "leaf"},
-            {"m", "monkey", "mountain", "moon"},
-            {"n", "nest", "nose", "notebook"},
-            {"o", "orange", "owl", "ocean"},
-            {"p", "pear", "penguin", "piano"},
-            {"q", "queen", "quilt", "quail"},
-            {"r", "rabbit", "rainbow", "rock"},
-            {"s", "sun", "star", "spider"},
-            {"t", "tiger", "tree", "turtle"},
-            {"u", "umbrella", "unicorn", "umpire"},
-            {"v", "violin", "van", "volcano"},
-            {"w", "whale", "window", "wolf"},
-            {"x", "xylophone", "xenon", "x-ray"},
-            {"y", "yak", "yellow", "yacht"},
-            {"z", "zebra", "zero", "zipper"}
+        String[] dictionary= {"apple", "ant", "arrow", "b", "banana",
+            "bird", "boat", "c", "cat", "car", "cup", "d", "dog",
+            "duck", "desk", "e", "elephant", "egg", "eagle", "f", "fish",
+            "frog", "flag", "g", "grapes", "goat", "guitar", "h", "hat", 
+            "house", "horse", "i", "ice", "island", "ink", "j", "jelly", "jump",
+            "jacket", "k", "kite", "king", "kangaroo", "l", "lion", "lamp",
+            "leaf", "m", "monkey", "mountain", "moon", "n", "nest", "nose", 
+            "notebook", "o", "orange", "owl","a", "ocean", "p", "pear", "penguin",
+            "piano", "q", "queen", "quilt", "quail", "r", "rabbit", "rainbow",
+            "rock", "s", "sun", "star", "spider", "t", "tiger", "tree", "turtle",
+            "u", "umbrella", "unicorn", "umpire", "v", "violin", "van", "volcano",
+            "w", "whale", "window", "wolf", "x", "xylophone", "xenon", "x-ray", "y",
+            "yak", "yellow", "yacht", "z", "zebra", "zero", "zipper"
         };
         
-        for(String[] element:dictionary){
-            String letter=element[0];
-            ArrayList<String> words=new ArrayList<>(Arrays.asList(Arrays.copyOfRange(element, 1, element.length)));
+        ArrayList<String> list=new ArrayList<>();
+         for(String element:dictionary){
+            list.add(element);  
+        }
+         Collections.sort(list);
+        for(int i=0; i<list.size();i+=4){
+            String letter=list.get(i);
+            ArrayList<String> words=new ArrayList<>();
+            words.add(list.get(i+1));
+            words.add(list.get(i+2));
+            words.add(list.get(i+3));
             alphapet.put(letter, words);
         }
         
@@ -63,7 +59,7 @@ public class AlphapetDectionary {
             System.out.println(key+"      "+value);
         }
         
-        String special="r";
+        String special="w";
         System.out.println(alphapet.get(special));
     }
     
